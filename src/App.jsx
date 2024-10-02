2af55529b5f26f1e653c8d0d7f7ca1ab805a7a01
Mohd Faiz <faiz2929ansari@gmail.com>
2024-10-02 17:48:11 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,25 @@
+import React from 'react'
+import {BrowserRouter as Router, Routes, Route} from 'react-router-dom';
+import { Toaster } from 'react-hot-toast';
+import Home from './Pages/Home/Home';
+import NotFound from './Pages/NotFound/NotFound';
+import Success from './Pages/Success/Success';
+import './App.css'
+import Menu from './components/Menu';
+const App = () => {
+  return (
+    <>
+      <Router>
+        <Routes>
+          <Route path='/' element={<Home/>}/>
+          <Route path='/success' element={<Success/>}/>
+          <Route path='/menu' element={<Menu/>}/>
+          <Route path='*' element={<NotFound/>}/>
+        </Routes>
+        <Toaster/>
+      </Router>
+    </>
+  )
+}
+
+export default App
---
+++ src/components/Footer.jsx
@@ -0,0 +1,30 @@
+import React from "react";
+
+const Footer = () => {
+  return (
+    <footer>
+      <div className="container">
+        <div className="banner">
+          <div className="left">XYZEE</div>
+          <div className="right">
+            <p>Connaught Place, New Delhi, Delhi 110001
+
+</p>
+            <p>Open: 11:00 AM - 4:00 PM & 7:00 PM - 11:00 PM</p>
+          </div>
+        </div>
+        <div className="banner">
+          <div className="left">
+            <p></p>
+          </div>
+          <div className="right">
+            <p>All Rights Reserved.</p>
+          </div>
+        </div>
+      </div>
+    </footer>
+  );
+};
+
+
+export default Footer;
